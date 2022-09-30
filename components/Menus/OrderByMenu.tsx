3faf5f6d2b2dd2fd384cce0d8a65b1320157396d
Victor Wu <wuvictor95@gmail.com>
2022-09-30 10:55:06 -0700

    Show Chevron down on OrderByMenu

--- components/Menus/OrderByMenu.tsx
+++ components/Menus/OrderByMenu.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import BaseMenu from './BaseMenu';
 import { order_by_config } from '@/constants/index';
 import { FiltersContextType, OrderByKey, useFilters } from '@/providers/FiltersProvider';
-import { Check } from 'react-feather';
+import { Check, ChevronDown } from 'react-feather';
 
 const OrderByMenu: React.FC = () => {
   const [orderByLabel, setOrderByLabel] = useState(order_by_config.newestListing.label);
@@ -20,7 +20,12 @@ const OrderByMenu: React.FC = () => {
     dispatch({ type: 'set_order_by_key', payload: orderByKey });
   };
 
-  const ButtonChild = <div className="btn-secondary min-w-[125px]">{orderByLabel}</div>;
+  const ButtonChild = (
+    <div className="btn-secondary min-w-[125px] flex items-center space-x-2">
+      <span>{orderByLabel}</span>
+      <ChevronDown />
+    </div>
+  );
 
   const MenuItems = Object.values(order_by_config).map(({ label }, index) => {
     const isSelected = label === orderByLabel;
