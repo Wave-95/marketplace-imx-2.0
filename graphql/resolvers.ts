46d07edb55385d5ccf7f9a7840a630227ade5860
Victor Wu <wuvictor95@gmail.com>
2022-10-21 09:07:54 -0700

    Revert "Add graphql"
    This reverts commit 7bef828beea4779bd278d036fe4ff1f440eadae7.

--- graphql/resolvers.ts
+++
@@ -1,8 +0,0 @@
-import { DateTimeResolver } from 'graphql-scalars';
-
-export const resolvers = {
-  Query: {
-    users: async (_parent, _args, ctx) => await ctx.prisma.user.findMany(),
-  },
-  DateTime: DateTimeResolver,
-};
